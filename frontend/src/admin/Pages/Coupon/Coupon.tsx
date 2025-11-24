e0820e3f1b3403d2220733bdf1e8dd95481d28e7
Abdul khan <abdulkhan333c@gmail.com>
2025-11-24 21:40:12 +0530

    Add files via upload

---
+++ frontend/src/admin/Pages/Coupon/Coupon.tsx
@@ -0,0 +1,96 @@
+import { Delete } from '@mui/icons-material';
+import { Button, FormControl, InputLabel, MenuItem, Paper, Select, styled, Table, TableBody, TableCell, tableCellClasses, TableContainer, TableHead, TableRow } from '@mui/material';
+import React, { useState } from 'react';
+
+const accountStatus = [ // Renamed the original array
+  { status: 'PENDING_VERIFICATION', title: 'Pending Verification ', description: 'Account Verification Pending' },
+  { status: 'ACTIVE', title: 'Active ', description: 'Account is active and in good status' },
+  { status: 'SUSPENDED', title: 'Suspended ', description: 'Account is temporarily suspended' },
+  { status: 'DEACTIVATED', title: ' Deactivated ', description: 'Account is Deactivated' },
+  { status: 'BANNED', title: 'Banned ', description: 'Account is permanantly banned , due to Isshu' },
+  { status: 'CLOSED', title: 'close ', description: 'Account is permanantly Closeed' },
+];
+
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+
+const Coupon = () => {
+  const [selectedStatus, setSelectedStatus] = useState('ACTIVE'); // Renamed the state variable
+
+  const handleChange = (event: any) => {
+    setSelectedStatus(event.target.value); // Updated state variable name
+  };
+
+  return (
+  <>
+
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+          <StyledTableCell>Coupon Code</StyledTableCell>
+            <StyledTableCell>Start Date</StyledTableCell>
+            <StyledTableCell>End Date</StyledTableCell>
+            <StyledTableCell align="right">Minimum Order Value</StyledTableCell>
+            <StyledTableCell align="right">Discount</StyledTableCell>
+            <StyledTableCell align="right"> Delete</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {row.name}
+              </StyledTableCell>
+              <StyledTableCell >{row.calories}</StyledTableCell>
+              <StyledTableCell align="right">{row.fat}</StyledTableCell>
+              <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+              <StyledTableCell align="right">{row.protein}</StyledTableCell>
+              <StyledTableCell align="right"> <Button><Delete/></Button></StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  </>
+  );
+};
+
+export default Coupon;
