d3b4fbc203146a2fe30e0651740b5faafeb4cbea
Abdul khan <abdulkhan333c@gmail.com>
2025-11-24 22:53:44 +0530

    Add files via upload

---
+++ backend/src/main/java/com/zosh/request/CreateReviewRequest.java
@@ -0,0 +1,15 @@
+package com.zosh.request;
+
+import java.util.List;
+
+import lombok.Data;
+
+
+@Data
+public class CreateReviewRequest {
+
+    private String reviewText;
+    private double reviewRating;
+    private List<String> productImages;
+    
+}
